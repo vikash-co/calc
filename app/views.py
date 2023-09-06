6a797f75a587f477cd33f240d4999cbd76a9714b
vikash-co <vs292382@gmail.com>
2023-09-06 07:21:35 +0530

    first change

--- app/views.py
+++ app/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from datetime import datetime
+
 from django.http import HttpResponse
+
 def index(request):
-    return HttpResponse("Hello World")
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Vercel!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
